d5c4b7131caab5e486d67b4375e42caa711a1387
Benyamin Keshavarz <77743256+BenyaminKeshavarz@users.noreply.github.com>
2021-01-31 16:01:29 +0330

    Update TestC#.cs

--- TestC#.cs
+++ TestC#.cs
@@ -7,6 +7,7 @@ namespace Git
         static void Main(string[] args)
         {
             Console.WriteLine("hello, world!");
+            Console.WriteLine("welcome!");
             Console.ReadKey();
         }
     }
